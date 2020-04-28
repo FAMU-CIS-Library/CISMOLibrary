4e245fa671e05bde603d36f52bc93f72c9b249c0
Freddie <frederic.triplett@gmail.com>
2020-04-28 08:26:20 -0400

    Add files via upload

--- app/src/main/java/com/example/cismolibrary/LoginActivity.java
+++ app/src/main/java/com/example/cismolibrary/LoginActivity.java
@@ -3,14 +3,50 @@ package com.example.librarytimeline;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.parse.LogInCallback;
+import com.parse.ParseException;
+import com.parse.ParseUser;
 
 public class LoginActivity extends AppCompatActivity {
+
+    public static final String TAG = "LoginActivity";
+    private EditText etStudentID;
+    private EditText etPassword;
+    private Button btnRegister;
+    private Button btnLogin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-        // Sample test comment
+        etStudentID = findViewById(R.id.etStudentNumber);
+        etPassword = findViewById(R.id.etPassword);
+        btnLogin = findViewById(R.id.btnLogin);
+        btnRegister = findViewById(R.id.btnRegister);
+
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String studID = etStudentID.getText().toString();
+                String password = etPassword.getText().toString();
+
+                loginStudent(studID, password);
+            }
+        });
+    }
+
+    private void loginStudent(String studID, String password) {
+        ParseUser.logInInBackground(studID, password, new LogInCallback() {
+            @Override
+            public void done(ParseUser user, ParseException e) {
+
+            }
+        });
     }
 }
